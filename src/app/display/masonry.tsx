201e5a6dfbdc0c71b3d85047ed2df8a1e54c98c2
jkjan <ytom2607@gmail.com>
2024-12-14 12:38:05 +0900

    MasonryInfiniteScroller 는 괜찮으나 flickering 현상 있음

--- src/app/display/masonry.tsx
+++ src/app/display/masonry.tsx
@@ -1,92 +1,67 @@
-'use client';
-import Masonry from '@mui/lab/Masonry';
-import * as React from 'react';
-import ImageModal from './modal';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { useEffect } from 'react';
-import { Post } from '@/app/api/post/route';
-import { Box, CircularProgress } from '@mui/material';
+'use client'
+import { MasonryInfiniteGrid } from "@egjs/react-infinitegrid";
 
-type PostResponseDTO = {
-  posts: Post[],
-  isLast: boolean
+import Image from "next/image";
+import {useState} from "react";
+import MasonryInfiniteScroller from 'react-masonry-infinite';
+
+export interface GroupData {
+  nextGroupKey: number;
+  count: number;
 }
 
-export default function ImageMasonry() {
-  let lastCreatedAt = {
-    createdAt: 'null',
-  };
-  let hasMore = true;
+function getItems({ nextGroupKey, count }:GroupData) {
+  const nextItems = [];
+  const nextKey = nextGroupKey * count;
 
-  const [open, setOpen] = React.useState(false);
-  const [imageData, setImageData] = React.useState<Post[]>([]);
-  const [image, setImage] = React.useState('');
-  const masonryRef = React.useRef<any>(null);
-  const handleClose = () => setOpen(false);
-  const handleImage = (value: string) => {
-    setImage(value);
-    setOpen(true);
-  };
-
-  const fetchMore = async () => {
-    console.log('Fetching more');
-    const params = new URLSearchParams(lastCreatedAt);
-    const response = fetch(`/api/post/?${params.toString()}`, {
-      method: 'GET',
+  for (let i = 0; i < count; ++i) {
+    nextItems.push({
+      groupKey: nextGroupKey,
+      key: nextKey + i
     });
-
-    response
-      .then(async response => {
-        if (response.ok) {
-          const { posts, isLast }: PostResponseDTO = await response.json();
-          hasMore = !isLast;
-          lastCreatedAt.createdAt = posts[posts.length - 1].createdAt;
-          setImageData(prevState => [...prevState, ...posts]);
-          console.log(posts);
-        }
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  };
-
-  const forceUpdateLayout = () => {
-    if (masonryRef.current) {
-      // masonryRef.current.update();
-    }
   }
+  return nextItems;
+}
 
-  // useEffect 내에서 비동기 함수 호출
-  useEffect(() => {
-    fetchMore();
-  }, []);  // 빈 배열을 의존성 배열로 사용하여 최초 렌더링 시 한 번만 호출
+const Item = ({ num }: any) => {
+  return (
+    <div style={{width: "250px"}}>
+      <div>
+        <img
+          src={`https://naver.github.io/egjs-infinitegrid/assets/image/${(num % 33) + 1}.jpg`}
+          alt="egjs"
+          style={{
+            width: "100%",
+            height: "auto",
+            borderRadius: 8
+          }}
+          width={500}
+          height={500}
+        />
+      </div>
+      <div>{`egjs ${num}`}</div>
+    </div>
+  );
+}
+
+export default function Page() {
+  const [items, setItems] = useState(() => getItems({ nextGroupKey: 0, count: 10 }));
 
   return (
-    <div>
-      <Box id="scrolableDiv" sx={{ width: 800, height: 800, overflowY: 'scroll' }}>
-        <Masonry columns={{ xs: 3, sm: 4, md: 5 }} spacing={2} ref={masonryRef}>
-          <InfiniteScroll next={fetchMore} loader={hasMore ? <CircularProgress /> : null} hasMore={hasMore}
-                          dataLength={imageData.length}>
-            {imageData.map((item: Post) => (
-              <div key={item.postId}>
-                <img
-                  onClick={() => handleImage(item.imageUrl)}
-                  src={`${item.imageUrl}?width=300`}
-                  alt={item.imageUrl}
-                  loading="lazy"
-                  style={{
-                    borderRadius: 4,
-                    display: 'block',
-                    width: '100%',
-                    minHeight: 30,
-                  }}
-                />
-              </div>
-            ))}
-            <ImageModal open={open} handleClose={handleClose} image={image} />
-          </InfiniteScroll>
-        </Masonry>
-      </Box>
-    </div>
+    <MasonryInfiniteScroller
+      pageStart={0}
+      loadMore={(e) => {
+        const nextGroupKey = (+e.groupKey! || 0);
+
+        setItems([
+          ...items,
+          ...getItems({ nextGroupKey: nextGroupKey, count: 10 }),
+        ]);
+      }
+    }
+      hasMore={true}
+      loader={<div>{'Loading ...'}</div>}>
+      {items.map((item, index) => <Item data-grid-groupkey={item.groupKey} key={index} num={item.key} />)}
+    </MasonryInfiniteScroller>
   );
 }
