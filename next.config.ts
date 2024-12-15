1dfc4509d0f3e1d96753eb0574d74a8524d2d17e
jkjan <>
2024-12-15 02:02:05 +0000

    oidc 안 쓰고 secret key로

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   /* config options here */
-  swcMinify: false,
-  reactStrictMode: false,
 };
 
 if (process.env.NODE_ENV === "production") {
--- src/app/lib/dynamo-db.ts
+++ src/app/lib/dynamo-db.ts
@@ -1,21 +1,3 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { awsCredentialsProvider } from '@vercel/functions/oidc';
 
-const ENV = process.env.NODE_ENV || 'development';
-const AWS_REGION = process.env.AWS_REGION;
-const AWS_ROLE_ARN = process.env.AWS_ROLE_ARN;
-
-const credentials = () => {
-  if (ENV === 'development') {
-    return {};
-  } else {
-    return {
-      region: AWS_REGION,
-      credentials: awsCredentialsProvider({
-        roleArn: AWS_ROLE_ARN,
-      }),
-    };
-  }
-};
-
-export const db = new DynamoDBClient(credentials());
+export const db = new DynamoDBClient({});
