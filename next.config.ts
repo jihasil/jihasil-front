154bb4f6f4d41b2d86a99652a848b3103b32977b
jkjan <ytom2607@gmail.com>
2024-12-15 10:16:25 +0900

    swcminify 제거

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  swcMinify: true,
+  swcMinify: false,
 };
 
 if (process.env.NODE_ENV === "production") {
