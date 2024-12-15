5e3d22bb5030f0e9b295ca6a120b8172d33fe0ba
jkjan <ytom2607@gmail.com>
2024-12-15 09:29:31 +0900

    production 에서 oidc 사용하도록 수정

--- src/app/lib/dynamo-db.ts
+++ src/app/lib/dynamo-db.ts
@@ -1,3 +1,21 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { awsCredentialsProvider } from '@vercel/functions/oidc';
 
-export const db = new DynamoDBClient({});
+const ENV = process.env.NODE_ENV || 'development';
+const AWS_REGION = process.env.AWS_REGION;
+const AWS_ROLE_ARN = process.env.AWS_ROLE_ARN;
+
+const credentials = () => {
+  if (ENV === 'development') {
+    return {};
+  } else {
+    return {
+      region: AWS_REGION,
+      credentials: awsCredentialsProvider({
+        roleArn: AWS_ROLE_ARN,
+      }),
+    };
+  }
+};
+
+export const db = new DynamoDBClient(credentials());
