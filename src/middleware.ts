b53417a6728eef09638e9f192672066846bb3bc5
jkjan <ytom2607@gmail.com>
2025-02-06 21:16:02 +0900

    회원가입 환경 블락 해제

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,13 @@ import type { NextRequest } from "next/server";
 import { auth } from "@/auth";
 
 export async function middleware(request: NextRequest) {
-  // 개발환경에서의 회원가입 제외한 모든 POST 요청 제한
+  // 회원가입 제외한 모든 비인가 POST 요청 제한
   if (
     request.method === "POST" &&
     request.nextUrl.pathname.startsWith("/api")
   ) {
     const session = await auth();
-    if (
-      !session?.user &&
-      !(
-        request.nextUrl.pathname.startsWith("/api/user") &&
-        process.env.VERCEL_ENV !== "production"
-      )
-    ) {
+    if (!session?.user && !request.nextUrl.pathname.startsWith("/api/user")) {
       return new NextResponse("권한이 없습니다.", {
         status: 401,
       });
@@ -39,15 +33,7 @@ export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/user/signUp")) {
     const session = await auth();
 
-    if (!session?.user) {
-      // 개발자 서버 아니면 회원가입 제한
-      if (process.env.VERCEL_ENV === "production") {
-        return new NextResponse(
-          "회원가입을 하실 수 없습니다. 개발자에게 문의하세요.",
-          { status: 401 },
-        );
-      }
-    } else {
+    if (session?.user) {
       // 이미 로그인 돼있을 시 유저 페이지로 리다이렉트
       return NextResponse.redirect(new URL(`/user/myPage`, request.url));
     }
@@ -66,6 +52,7 @@ export async function middleware(request: NextRequest) {
   // 유저 페이지 제한
   if (request.nextUrl.pathname.startsWith("/user/myPage")) {
     const session = await auth();
+
     if (!session?.user) {
       const signInUrl = new URL("/user/signIn", request.url);
       signInUrl.searchParams.set("from", request.nextUrl.pathname);
