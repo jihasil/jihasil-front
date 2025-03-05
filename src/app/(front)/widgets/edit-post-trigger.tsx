acbbc8e38f146296bd0a1d994dd4e304422c6d47
jkjan <ytom2607@gmail.com>
2025-03-05 21:44:18 +0900

    delete 버튼 디자인 수정

--- src/app/(front)/widgets/edit-post-trigger.tsx
+++ src/app/(front)/widgets/edit-post-trigger.tsx
@@ -2,77 +2,37 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { toast } from "sonner";
 
 import { Button } from "@/app/(front)/components/ui/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/app/(front)/components/ui/dialog";
-import SubmitButton from "@/app/(front)/components/ui/submit-button";
+import { DeleteDialog } from "@/app/(front)/components/ui/delete-dialog";
 import { fetchR } from "@/app/(front)/shared/lib/request";
 
 export const EditPostTrigger = (props: { postId: string }) => {
   const { postId } = props;
-  const [isUploading, setIsUploading] = useState(false);
   const router = useRouter();
 
+  const onDelete = async () => {
+    const response = await fetchR(`/api/post/${postId}`, {
+      method: "DELETE",
+    });
+    console.log(response);
+    const body = await response.json();
+    toast.info(body.message);
+    router.push("/");
+  };
+
   return (
-    <>
+    <div className="grid grid-cols-2 gap-3">
       <Link
         href={{
           pathname: `/post/edit/${postId}`,
         }}
+        className="col-span-1"
       >
         <Button className="w-full">수정</Button>
       </Link>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="destructive">삭제</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>삭제하시겠습니까?</DialogTitle>
-            <DialogDescription className="my-3">
-              되돌릴 수 없습니다. 정말로 지우시겠습니까?
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <div className="flex gap-3">
-              <DialogClose asChild>
-                <Button>아니요</Button>
-              </DialogClose>
-              <SubmitButton
-                variant="destructive"
-                onClick={() => {
-                  setIsUploading(true);
-                  console.log(postId);
-                  fetchR(`/api/post/${postId}`, {
-                    method: "DELETE",
-                  })
-                    .then(async (response) => {
-                      const body = await response.json();
-                      toast.info(body.message);
-                      router.push("/");
-                    })
-                    .finally(() => {
-                      setIsUploading(false);
-                    });
-                }}
-                text="삭제"
-                isUploading={isUploading}
-              />
-            </div>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </>
+      <DeleteDialog deleteMethod={onDelete} className="col-span-1" />
+    </div>
   );
 };
